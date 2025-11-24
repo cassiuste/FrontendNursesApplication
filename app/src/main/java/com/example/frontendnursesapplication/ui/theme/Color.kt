b8edb2fbf96acff47ae42ea38cfaff217fe2cebf
Dianathecoder <diana.canyas15@gmail.com>
2025-11-24 23:24:53 +0100

    Creación login

--- app/src/main/java/com/example/frontendnursesapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/frontendnursesapplication/ui/theme/Color.kt
@@ -1,5 +1,8 @@
 package com.example.frontendnursesapplication.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +12,24 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+
+
+val LightBlueWhite = Color(0xFFF1F5F9)
+
+val BlueGray = Color(0xFF334155)
+
+val SoftRed = Color(0xFFD04A4A)
+
+val ColorScheme.focusedTextFieldText
+    @Composable
+    get() = if (isSystemInDarkTheme()) Color.White else Color.Black
+
+val ColorScheme.unfocusedTextFieldText
+    @Composable
+    get() = if (isSystemInDarkTheme()) Color(0xFF94A388) else Color(0xFF475569)
+
+val ColorScheme.textFieldContainer
+    @Composable
+    get() = if (isSystemInDarkTheme()) BlueGray.copy(alpha = 0.6f ) else LightBlueWhite
